0152bed30ba406e275a544a65f5d18f3e41cbe51
Nur Aziz <127321492+nuraziz1344@users.noreply.github.com>
2024-10-10 00:18:41 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -14,7 +14,7 @@ let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='
 // v2board api environment variables (optional) deprecated, please use planetscale.com instead
 
 if (!isValidUUID(userID)) {
-	throw new Error('uuid is invalid');
+	return new Response(e.toString('uuid is invalid'));;
 }
 
 export default {
